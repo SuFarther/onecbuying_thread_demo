c821a3f618fe97eb916f888c6e3b0b6ace2c69aa
xiaosu <2257346739@qq.com>
2022-07-21 20:32:09 +0800

    使用继承方式创建线程

---
+++ onecbuying_thread_demo01/src/com/onecbuying/ThreadDemo01.java
@@ -0,0 +1,12 @@
+package com.onecbuying;
+
+/**
+ * @author 苏东坡
+ * @version 1.0
+ * @ClassName ThreadDemo01
+ * @company 公司
+ * @Description 如何创建多线程
+ * @createTime 2022年07月21日 20:30:30
+ */
+public class ThreadDemo01 {
+}
