609eb808f3e64bfbcf2437d99b0ae85c51b3db1a
xiaosu <2257346739@qq.com>
2022-07-21 19:12:13 +0800

    多线程的入门demo下的用户实体类

---
+++ onecbuying_thread_batch/src/com/onecbuying/pojo/UserPojo.java
@@ -0,0 +1,32 @@
+package com.onecbuying.pojo;
+
+import java.io.Serializable;
+
+/**
+ * @author 苏东坡
+ * @version 1.0
+ * @ClassName UserPojo
+ * @company 公司
+ * @Description TODO
+ * @createTime 2022年07月21日 19:09:09
+ */
+public class UserPojo implements Serializable {
+    private  String userId;
+    private  String userName;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
