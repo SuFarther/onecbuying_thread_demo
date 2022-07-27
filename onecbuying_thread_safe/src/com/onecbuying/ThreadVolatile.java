fe0e6817416b36cc3deecf386a8cf6a318d96372
xiaosu <2257346739@qq.com>
2022-07-27 14:46:30 +0800

    一个线程使用同步函数,另一个线程使用同步代码块(非this)不能同步

--- onecbuying_thread_safe/src/com/onecbuying/ThreadVolatile.java
+++ onecbuying_thread_safe/src/com/onecbuying/ThreadVolatile.java
@@ -6,12 +6,9 @@ package com.onecbuying;
  * @ClassName ThreadVolatile
  * @company 公司
  * @Description
- *  public boolean flag = true;
- *  原先flag为true，存放在主内存中，存放的共享数据为flag = true
- *  因为主内存中有flag=true
- *  ThreadVolatileDemo开辟了一个线程,假设第一次为t1,在他本地私有内存中当走while(flag) {}那flag=true;然后t1把本地私有flag =  true刷新到
- *  主内存中，所以主内存中为true,后面threadVolatileDemo.setFlag(false);主线程修改值把值设置为false,主内存通知给t2
- *  两个线程之间操作是不可见的啊，加volatile让线程之间可见
+ *Volatile作用是保证两个线程之间的可见性,主内存中存放着共享数据public volatile boolean flag = true;当ThreadVolatileDemo这个线程子线程开始执行时,其实他原先本地内存就有一个本地私有内存 flag = true,当线程运行run方法后,执行while(flag) {}，flag = true把自己当本地私有内存的flag = true及时刷新到主内存中去,主内存得到值flag=tr
+ *ue,通知给主线程中的本地私有内存，ThreadVolatileDemo threadVolatileDemo = new ThreadVolatileDemo();主内存原先本地私有内存为flag= true;当他执行threadVolatileDemo.setFlag(false);主线程的本地私有内存为flag=false;
+ *子线程为true,主线程为false,主内存为true,我想让子线程访问主线程中的值，本来线程之间是不可见的，用Volatile让他可见v
  * @createTime 2022年07月27日 14:08:08
  */
 class ThreadVolatileDemo extends  Thread {
